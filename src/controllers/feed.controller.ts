8065f744924439a70e7cc04c8b5babfe6b47ff92
rakeshpdezigndia <rakeshkp683@gmail.com>
2023-01-25 16:27:58 +0530

    add feed Favorite's API

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -90,4 +90,18 @@ export class FeedController {
             return res.status(INTERNAL_SERVER_ERROR).send({error});
         }
     }
+
+    public static async addFeedFavourite(req: Request, res: Response) {
+        try {
+            const data: any = await feed.addFeedFavourite(req);
+            if (data.status) {
+                return res.status(StatusCodes.OK).json(data);
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).json(data);
+            }
+        } catch (error) {
+            return res.status(INTERNAL_SERVER_ERROR).send({error});
+        }
+    }
+    
 }
